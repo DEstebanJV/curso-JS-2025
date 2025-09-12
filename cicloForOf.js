b545d2402d12d12c536ec4693f83a1999f3b7916
destebanjv <davidjv312@gmail.com>
2025-09-12 14:05:11 -0500

    actualizacion de ciclos

---
+++ cicloForOf.js
@@ -0,0 +1,11 @@
+/* Ciclo for of 
+este es usado para iterar sobre arrays
+y objetos 
+por cada elemento de
+*/
+
+let list = [1, 2, 3, 4, 5,"pera", "manzana"];
+
+for (let i of list) {
+    console.log(i);
+}
---
+++ ciclos.js
@@ -0,0 +1,32 @@
+/* switch */
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
+/* for */
+
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+/* while */
+
+
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+/* do while */
+
+let i = 0;
+do {
+    
+} while (condition);
+
