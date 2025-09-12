b3c7ff04beec8e131f7947c757e884659d94865b
destebanjv <davidjv312@gmail.com>
2025-09-12 17:59:10 -0500

    Ciclos

--- cicloForOf.js
+++ cicloForOf.js
@@ -9,3 +9,27 @@ let list = [1, 2, 3, 4, 5,"pera", "manzana"];
 for (let i of list) {
     console.log(i);
 }
+
+/* Ciclo for in
+este es usado para iterar sobre objetos
+y arrays
+por cada propiedad de
+*/
+
+let lista = {
+    "pera": 1,
+    "manzana": 2,
+    "naranja": 3,
+    "uva": 4,
+    "mano": 5
+}
+
+for (let key in lista) {
+    console.log(key);
+    console.log(lista[key]);
+    console.log(`${key} ${lista[key]}`);
+
+}
+
+/* presentar los datos de la lista */
+
